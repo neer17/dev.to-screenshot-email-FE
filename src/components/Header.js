a67564a9f8d9c0fcff01693596e7dd7b884c00a5
neer17 <neerajsewanisrt8@gmail.com>
2021-10-26 09:35:52 +0530

    feat: Add UI

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div className="header_root">
+            <span>DASHBOARD</span>
+            <div class="export_btn">Export Screenshot</div>
+        </div>
+    )
+}
